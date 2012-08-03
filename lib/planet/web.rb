191b35fd6276cc12aa2e6cfab55f29ac9fa1961a
Michael Cetrulo <michael.cetrulo@globant.com>
2012-08-03 16:07:28 -0300

    navigation

--- lib/planet/web.rb
+++ lib/planet/web.rb
@@ -3,9 +3,27 @@ require 'haml'
 
 class Planet
   class Web < Sinatra::Base
-    get '/' do
+    helpers do
+      def navlink(name, path)
+        if @path == path
+          haml %Q[%span{:class => "current_path"}= "#{name}"]
+        else
+          haml %Q[%a{:href => "#{url(path)}"}= "#{name}"]
+        end
+      end
+    end
+
+    before do
       @conf = YAML.load_file('planet.yml')
-      haml :index
+      @path = request.path_info
+    end
+
+    get '/' do
+      haml :blogs
+    end
+
+    get '/settings' do
+      haml :settings
     end
   end
 end
